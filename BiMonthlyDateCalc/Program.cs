a7cc009aaddd631437981fbd79789dac03f6dc5d
aj-5001 <alecjasanovsky@gmail.com>
2018-05-07 21:08:13 -0400

    Commit 1
    current date created. just need to do action to currentDay

--- BiMonthlyDateCalc/Program.cs
+++ BiMonthlyDateCalc/Program.cs
@@ -10,6 +10,19 @@ namespace BiMonthlyDateCalc
     {
         static void Main(string[] args)
         {
+            DateTime today = DateTime.Today;
+            DateTime payDay = new DateTime();
+              
+            Console.WriteLine("Today is: " + today.Month + "/" + today.Day + "/" + today.Year);
+            payDay = getPayday(today);
+            Console.WriteLine("The end payday is: ");
+            Console.ReadKey();
+        }
+
+        static public DateTime getPayday(DateTime currentDay)
+        {
+            //do something to currentday
+            return currentDay;
         }
     }
 }
