cba520b5b30e108f4425cfde8c8609976844aef4
aj-5001 <alecjasanovsky@gmail.com>
2018-05-07 21:36:54 -0400

    Commit 2 - Finished
    Quick little program to tell the next bi-monthly payday

--- BiMonthlyDateCalc/Program.cs
+++ BiMonthlyDateCalc/Program.cs
@@ -8,21 +8,36 @@ namespace BiMonthlyDateCalc
 {
     class Program
     {
+        static DateTime today = DateTime.Today;
+        static DateTime payDay = new DateTime();
+        static int daysTilPaid = 0;
+
         static void Main(string[] args)
         {
-            DateTime today = DateTime.Today;
-            DateTime payDay = new DateTime();
-              
+                 
             Console.WriteLine("Today is: " + today.Month + "/" + today.Day + "/" + today.Year);
             payDay = getPayday(today);
-            Console.WriteLine("The end payday is: ");
+            Console.WriteLine("The end payday is: " + payDay.Month + "/" + payDay.Day + "/" + payDay.Year);
+            Console.WriteLine("You get paid in " + daysTilPaid + " Days");
             Console.ReadKey();
         }
 
         static public DateTime getPayday(DateTime currentDay)
         {
             //do something to currentday
-            return currentDay;
+            if (currentDay.Day < 15)
+            {
+                daysTilPaid = 15 - currentDay.Day;
+                currentDay = currentDay.AddDays(daysTilPaid);
+                return currentDay;
+            }
+            else
+            {
+                daysTilPaid = 31 - currentDay.Day;
+                currentDay.AddDays(daysTilPaid);
+                return currentDay;
+            }
+            
         }
     }
 }
